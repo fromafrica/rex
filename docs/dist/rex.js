ed5dd14e7eef1f25515d51b652f30ba66172c47e
Sami Fouad <sfouad@gmail.com>
2023-07-23 01:36:58 -0600

    removing TS, organizing code

--- docs/dist/rex.js
+++ docs/dist/rex.js
@@ -1,22 +1,37 @@
 // global app state
 const { appState } = await import('./state.js');
 window.appState = appState;
+
 //import * as helper from './rex/helper.ts'
+
+// default components
+const { RexHome, RexHelp, RexError } = await import('./defaults.js');
+
+// component definitions
+window.customElements.define("rex-home", RexHome);
+window.customElements.define("rex-help", RexHelp);
+window.customElements.define("rex-error", RexError);
+
 // routing
 const { getPath, checkPath, renderComponent } = await import('./router.js');
+
 // get
 const path = getPath();
+
 // define
 window.appState.setRouter({
-    home: '<rex-home></rex-home>',
-    help: '<rex-help></rex-help>',
+	home: '<rex-home></rex-home>',
+	help: '<rex-help></rex-help>',
+	error: '<rex-error></rex-error>',
 });
+
 // validate + display
 const router = window.appState.getRouter();
 if (checkPath(Object.keys(router), path)) {
-    document.querySelector("#app").innerHTML = renderComponent(router[path[0]]);
+	document.querySelector("#app").innerHTML = renderComponent(router[path[0]]);
+} else {
+	document.querySelector("#app").innerHTML = "<rex-error></rex-error>";
 }
-else {
-    document.querySelector("#app").innerHTML = "<error-page></error-page>";
-}
+
+// modules require an export, even if it's empty
 export {};
